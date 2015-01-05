eb462743d2dd000e0e8b72d7d019d8e17aaf7810
iZaQ <skuvvon@gmail.com>
2015-01-05 02:25:32 +0000

    Standalone Orbwalker

---
+++ Standalone Orbwalker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Standalone_Orbwalker
+{
+    class Program
+    {
+        private static Orbwalking.Orbwalker _orbwalker;
+        private static Menu _menu;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            _menu = new Menu("Standalone Orbwalker","Orb",true);
+            var orbMenu = new Menu("Orbwalker", "orbStand");
+            _orbwalker = new Orbwalking.Orbwalker(orbMenu);
+            _menu.AddSubMenu(orbMenu);
+            _menu.AddToMainMenu();
+        }
+    }
+}
